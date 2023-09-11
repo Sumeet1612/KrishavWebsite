21339d7823656f2b7cdeaec13ad51bd61b6cbdc6
Sumeet <sumeet.singh1612@gmail.com>
2023-09-11 13:06:23 -0400

    added New Service

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,12 +3,12 @@ import React from "react";
 const Services = () => {
   return (
     <section id="services" className="services">
-      <h2>Our Services</h2> 
+      <h2>Our Services</h2>
       <p>
         At Krishav Enterprise, we offer a comprehensive range of services to
         cater to your security and electrical needs:
       </p>
-      <br/>
+      <br />
 
       <div className="MasterCard">
         <div className="card slide-card">
@@ -58,39 +58,52 @@ const Services = () => {
           </div>
         </div>
 
-        {/* <div className="BoxCenter"> */}
-          <div className="card slide-card">
-            <div className="card-body">
-              <h5
-                className="card-title"
-                style={{ fontSize: "24px", fontWeight: "bold" }}
-              >
-                Emergency Services
-              </h5>
-              <p className="card-text">
-                We understand that electrical emergencies can happen at any
-                time. That's why we offer 24/7 emergency electrical services to
-                get your systems up and running promptly.
-              </p>
-            </div>
+        <div className="card slide-card">
+          <div className="card-body">
+            <h5
+              className="card-title"
+              style={{ fontSize: "24px", fontWeight: "bold" }}
+            >
+              Emergency Services
+            </h5>
+            <p className="card-text">
+              We understand that electrical emergencies can happen at any time.
+              That's why we offer 24/7 emergency electrical services to get your
+              systems up and running promptly.
+            </p>
           </div>
+        </div>
 
-          <div className="card slide-card">
-            <div className="card-body">
-              <h5
-                className="card-title"
-                style={{ fontSize: "24px", fontWeight: "bold" }}
-              >
-                Maintenance and Upgrades
-              </h5>
-              <p className="card-text">
-                Keep your systems running smoothly with our maintenance
-                services, and enhance their performance with our upgrade
-                solutions.{" "}
-              </p>
-            </div>
+        <div className="card slide-card">
+          <div className="card-body">
+            <h5
+              className="card-title"
+              style={{ fontSize: "24px", fontWeight: "bold" }}
+            >
+              Maintenance and Upgrades
+            </h5>
+            <p className="card-text">
+              Keep your systems running smoothly with our maintenance services,
+              and enhance their performance with our upgrade solutions.{" "}
+            </p>
           </div>
-        {/* </div> */}
+        </div>
+
+        <div className="card slide-card">
+          <div className="card-body">
+            <h5
+              className="card-title"
+              style={{ fontSize: "24px", fontWeight: "bold" }}
+            >
+              Home Automation Solutions
+            </h5>
+            <p className="card-text">
+              magine having complete control over your home's lighting, climate,
+              security, and entertainment systems with a single touch or voice
+              command.
+            </p>
+          </div>
+        </div>
         <br />
       </div>
     </section>
